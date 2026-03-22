bfcfc3c4e270f98a3c56cdefc5c1e31c8d1d3a0a
2k33cse992574 <sharmaarin87@gmail.com>
2026-03-22 11:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (12.28%) - LeetSync

--- 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.cpp
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool digitCount(string num) {
-        unordered_map<int,int> mp;
+        unordered_map<int, int> mp;
         vector<int> n(num.size());
-        for(int i=0;i<num.size();i++)
-        {
-            n[i]=num[i]-'0';
+        for (int i = 0; i < num.size(); i++) {
+            n[i] = num[i] - '0';
         }
-        for(int x:n)
-        {
+        for (int x : n) {
             mp[x]++;
         }
-        for(int i=0;i<n.size();i++)
-        {
-            if(mp[i]!=n[i])
-            {
+        for (int i = 0; i < n.size(); i++) {
+            if (mp[i] != n[i]) {
                 return false;
             }
         }
