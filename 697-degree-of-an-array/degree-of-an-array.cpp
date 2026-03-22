7b8d32336f8997276afac039a8eb11e2ef5fd190
2k33cse992574 <sharmaarin87@gmail.com>
2026-03-22 14:38:15 +0530

    Time: 7 ms (85.58%) | Memory: 30.1 MB (30.99%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.cpp
+++ 697-degree-of-an-array/degree-of-an-array.cpp
@@ -9,31 +9,20 @@ public:
             if (mp1.find(nums[i]) == mp1.end()) {
                 mp1[nums[i]] = i;
             }
-        }
-        for (int i = 0; i < nums.size(); i++) {
             mp2[nums[i]] = i;
-        }
-        vector<int> arr;
-        int maxi = INT_MIN;
-        vector<int> maximum;
-        int maxim = 0;
-        for (int i = 0; i < nums.size(); i++) {
             freq[nums[i]]++;
         }
+        int maxi = 0;
         for (auto& it : freq) {
-            if (it.second >= maxi) {
-                maxi = it.second;
-                maxim = it.first;
-            }
+            maxi = max(maxi, it.second);
         }
+
         for (auto& it : freq) {
             if (it.second == maxi) {
-                maximum.push_back(it.first);
+                ans = min(ans, mp2[it.first] - mp1[it.first] + 1);
             }
         }
-        for (int i = 0; i < maximum.size(); i++) {
-            ans = min(ans, mp2[maximum[i]] - mp1[maximum[i]] + 1);
-        }
+
         return ans;
     }
 };
