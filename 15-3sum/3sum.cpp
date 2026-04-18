64cc3e2508006b3f10e52e972488bfe238640ef9
2k33cse992574 <sharmaarin87@gmail.com>
2026-04-18 20:43:04 +0530

    Time: 48 ms (55.46%) | Memory: 29 MB (72.11%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,24 +3,23 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
+        for (int k = 0; k < nums.size(); k++) {
+            if (k > 0 && nums[k] == nums[k - 1])
                 continue;
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k) {
+            int i = k + 1, j = nums.size() - 1;
+            while (i < j) {
                 if (nums[i] + nums[j] + nums[k] == 0) {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    while (j < k && nums[j] == nums[j + 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k - 1])
-                        k--;
-                    j++;
-                    k--;
+                    ans.push_back({nums[k], nums[i], nums[j]});
+                    while ((i < j) && nums[i] == nums[i + 1])
+                        i++;
+                    while (i < j && nums[j] == nums[j - 1])
+                        j--;
+                    i++;
+                    j--;
                 } else if (nums[i] + nums[j] + nums[k] < 0) {
-                    j++;
+                    i++;
                 } else {
-                    k--;
+                    j--;
                 }
             }
         }
